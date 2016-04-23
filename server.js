eb6c4504f731c653dd58dd80a369d5b46a8ea1ff
maliaoreta <maliaoreta@gmail.com>
2016-04-22 16:31:18 -1000

    has post/gallery, and get/gallery

--- server.js
+++ server.js
@@ -1,1 +1,41 @@
-hello?
+'use strict'
+const express = require('express'),
+      app = express(),
+      bodyParser = require('body-parser'),
+      db = require('./models'),
+      Gallery = db.Gallery,
+      Photo = db.Photo;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/gallery', (req,res) => {
+  Photo.create({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  })
+  .then((photo) => {
+    res.json(photo);
+  });
+});
+
+app.post('/gallerys', (req,res) => {
+  Gallery.create({
+    title: req.body.title
+  })
+  .then((gallery) => {
+    res.json(gallery);
+  })
+});
+
+app.get('/', (req, res) => {
+  Photo.findAll()
+  .then((photos) => {
+    res.json(photos);
+  });
+});
+
+app.listen(3000, () => {
+  db.sequelize.sync();
+  console.log('Listening on 3000!');
+});
