5da8ea7a7e9dfaa98d47a2c50bf9ee1f731f311c
Laura <lg3295@gmail.com>
2016-04-26 12:06:47 -1000

    adds additional default photos

--- seeders/20160426093926-defaultPhotos.js
+++ seeders/20160426093926-defaultPhotos.js
@@ -17,6 +17,22 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date(),
       GalleryId: 1
+    },
+    {
+      author: 'Malia',
+      link: 'http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg',
+      description: 'puppy',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      GalleryId: 1
+    },
+    {
+      author: 'Laura',
+      link: 'https://36.media.tumblr.com/d0e0bac8efd8d67942a251f7e0f04881/tumblr_mzr7xnSRfT1s1rlwwo1_500.jpg',
+      description: 'baby deer',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      GalleryId: 1
     }], {});
   },
 
@@ -26,6 +42,12 @@ module.exports = {
     },
     {
       link: 'https://s-media-cache-ak0.pinimg.com/236x/8d/a1/02/8da1022e0e07d38b594cf6a2ef307a78.jpg'
+    },
+    {
+      link: 'http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg'
+    },
+    {
+      link: 'https://36.media.tumblr.com/d0e0bac8efd8d67942a251f7e0f04881/tumblr_mzr7xnSRfT1s1rlwwo1_500.jpg'
     }], {});
   }
 };
