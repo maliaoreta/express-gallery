28faa17c6fea12e2e9b1afb3ed37c68763497ae8
Laura <lg3295@gmail.com>
2016-04-26 11:29:51 -1000

    configures gulp to start server in nodemon and inject transpiled scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,32 @@
 'use strict';
 
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var nodemon = require('gulp-nodemon');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-  browserSync.init({
-      server: "./public"
-  });
+  // browserSync.init({
+  //     server: "./public"
+  // });
 
   gulp.watch("scss/**/*.scss", ['sass']);
-  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
+
+
+gulp.task('start', function () {
+  nodemon({
+    script : 'server.js',
+  });
+});
+
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("./public/css"))
-    .pipe(browserSync.stream());
+    // .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve', 'start']);
