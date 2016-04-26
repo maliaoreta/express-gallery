a00259797e0099efb94231fdc55fba497eca27fa
maliaoreta <maliaoreta@gmail.com>
2016-04-25 20:05:11 -1000

    creates migration from photo model

---
+++ models/photo.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Photo = sequelize.define('Photo', {
+    author: DataTypes.STRING,
+    link: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Photo.belongsTo(models.Gallery);
+      }
+    }
+  });
+  return Photo;
+};
