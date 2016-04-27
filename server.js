7b6d0b037c1c87c88da8950ca08f7f5a2b791203
maliaoreta <maliaoreta@gmail.com>
2016-04-26 18:33:41 -1000

    comments all other routes back in

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get('/', (req, res) => {
 });
 
 // Get a new photo form
-// app.get('/gallery/new', (req,res) => {
-//   res.render('./newPhoto');
-// });
+app.get('/gallery/new', (req,res) => {
+  res.render('./newPhoto');
+});
 
 // Get a particular photo
 app.get('/gallery/:id', function (req, res) {
@@ -47,7 +47,6 @@ app.get('/gallery/:id', function (req, res) {
     let photo = photos.filter((photoItem) => {
       return photoItem.id === Number(req.params.id)
     });
-    
     res.render('./photoDetail', {photo: photo[0], photoArr: photoArr});
   })
   .catch((error) => {
@@ -56,74 +55,74 @@ app.get('/gallery/:id', function (req, res) {
 });
 
 // //Edit photo form
-// app.get('/gallery/:id/edit', (req, res) => {
-//   Photo.findAll({
-//     where : {
-//       id : req.params.id
-//     }
-//   })
-//   .then((photo) => {
-//     res.render('./editPhoto', {photo : photo[0]});
-//   });
-// });
+app.get('/gallery/:id/edit', (req, res) => {
+  Photo.findAll({
+    where : {
+      id : req.params.id
+    }
+  })
+  .then((photo) => {
+    res.render('./editPhoto', {photo : photo[0]});
+  });
+});
 
 // // Posting a new photo
-// app.post('/gallery', (req,res) => {
-//   Photo.create({
-//     author: req.body.author,
-//     link: req.body.link,
-//     description: req.body.description,
-//     GalleryId: req.body.GalleryId
-//   })
-//   .then((photo) => {
-//     return res.json(photo);
-//   });
-// });
+app.post('/gallery', (req,res) => {
+  Photo.create({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description,
+    GalleryId: req.body.GalleryId
+  })
+  .then((photo) => {
+    return res.json(photo);
+  });
+});
 
 // // Post to create a new gallery
-// app.post('/gallerys', (req,res) => {
-//   Gallery.create({
-//     title: req.body.title
-//   })
-//   .then((gallery) => {
-//     return res.json(gallery);
-//   });
-// });
+app.post('/gallerys', (req,res) => {
+  Gallery.create({
+    title: req.body.title
+  })
+  .then((gallery) => {
+    return res.json(gallery);
+  });
+});
 
 // // '/gallery/:id/edit' route redirects here to do the editing
-// app.put('/gallery/:id', (req, res) => {
-//   Photo.update({
-//     author: req.body.author,
-//     link: req.body.link,
-//     description: req.body.description
-//   }, {
-//     where: {
-//       id: req.params.id
-//     }
-//   })
-//   .then(() => {
-//     res.redirect('/');
-//   })
-//   .catch((error) => {
-//     res.send(error);
-//   });
-// });
+app.put('/gallery/:id', (req, res) => {
+  Photo.update({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+});
 
 // //Delete :)
-// app.delete('/gallery/:id', (req, res) => {
-//   console.log('I am deleting things!');
-//   Photo.destroy({
-//     where : {
-//       id : req.params.id
-//     }
-//   })
-//   .then(() => {
-//     res.redirect('/');
-//   })
-//   .catch((error) => {
-//     res.send(error);
-//   });
-// })
+app.delete('/gallery/:id', (req, res) => {
+  console.log('I am deleting things!');
+  Photo.destroy({
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch((error) => {
+    res.send(error);
+  });
+})
 
 app.listen(3000, () => {
   db.sequelize.sync();
