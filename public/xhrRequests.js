53f4a84a7c7f3174ee58965ee51214c8bcf7db73
Laura <lg3295@gmail.com>
2016-04-28 12:15:15 -1000

    adds xhr request for registration form

--- public/xhrRequests.js
+++ public/xhrRequests.js
@@ -24,12 +24,32 @@ const deleteBtn = document.getElementById('deleteBtn');
 deleteBtn.addEventListener('click', (event) => {
   event.preventDefault();
 
+
   let xhrUrl = document.getElementById('xhrURL').value;
   const dReq = new XMLHttpRequest();
   dReq.addEventListener('load', (event) => {
     window.location = event.currentTarget.responseURL;
   });
+
   dReq.open("delete", xhrUrl);
   dReq.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
   dReq.send();
+});
+
+//xhr request for registration
+const registerBtn = document.getElementById('register');
+registerBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  let regInputsArr = event.currentTarget.parentNode.querySelectorAll('input[name]');
+  let regInfo = 'first_name=' + regInputsArr[0].value + '&username=' + regInputsArr[1].value + '&password=' + regInputsArr[2].value + '&passwordVal=' + regInputsArr[3].value;
+
+  const rReq = new XMLHttpRequest();
+  rReq.addEventListener('load', (event) => {
+    window.location = event.currentTarget.responseURL;
+  });
+
+  rReq.open("post", '/register');
+  rReq.setRequestHeader('Content-Type', "application/x-www-form-urlencoded");
+  rReq.send(regInfo);
 });
