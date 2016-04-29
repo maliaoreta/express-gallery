4e9b2b347e17f5bc2173d44621da27991ea16c37
Laura <lg3295@gmail.com>
2016-04-28 15:01:47 -1000

    adjusts onreadystatechange function to edit innerhtml of error div in jade file

--- public/registerXHR.js
+++ public/registerXHR.js
@@ -4,7 +4,7 @@
 const registerBtn = document.getElementById('register');
 registerBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  
+
   let regInputsArr = event.currentTarget.parentNode.querySelectorAll('input[name]');
   let regInfo = 'first_name=' + regInputsArr[0].value + '&username=' + regInputsArr[1].value + '&password=' + regInputsArr[2].value + '&passwordVal=' + regInputsArr[3].value;
 
@@ -14,8 +14,14 @@ registerBtn.addEventListener('click', (event) => {
 
   rReq.onreadystatechange = function () {
     if (rReq.readyState == 4 && rReq.status != 200) {
-      const wrongPassword = document.getElementById('wrongPassword');
-      wrongPassword.innerHTML = 'Passwords do not match!';
+      let res = JSON.parse(this.responseText);
+      let errorDiv = document.getElementById('errorDiv');
+      if (res.failure === "password"){
+        errorDiv.innerHTML = 'Passwords do not match!';
+      }
+      else if (res.failure === "username") {
+        errorDiv.innerHTML = "Username already exists"
+      }
     }
   }
 
