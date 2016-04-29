bdb5c2e1d1e5bf5bd097f26264e5058b2b5b342a
maliaoreta <maliaoreta@gmail.com>
2016-04-28 14:16:48 -1000

    checks if both password entries match

--- middleware/passwordVal.js
+++ middleware/passwordVal.js
@@ -4,7 +4,9 @@ function passwordVal () {
   return (req, res, next) => {
 
     let regInfo = req.body;
-    
+    if (!(regInfo.password === regInfo.passwordVal)) {
+      return res.status(400).send('no');
+    }
 
     return next();
   }
